66cae05e798edd9f6d2e2b38df4c965c0e0b5bbe
Александр Белкин <datum@datum.datum>
2024-01-10 20:19:35 +0300

    day 14

--- src/entities/board/lib/chessMoving.ts
+++ src/entities/board/lib/chessMoving.ts
@@ -1,5 +1,15 @@
 import { Cell, Coordinate } from "../model/types";
 
+interface MoveParams {
+  start: Coordinate;
+  target: Coordinate;
+  color: string;
+  cells: Map<string, Cell>;
+  targetCell: Cell;
+  targetElement: HTMLElement;
+  targetFigure: HTMLElement | null;
+}
+
 export const isComplies = (
   startCell: Cell,
   targetCell: Cell,
@@ -17,92 +27,132 @@ export const isComplies = (
   };
   if (!startCell.figure) return false;
 
+  const moveParams = {
+    start,
+    target,
+    color: startCell.figure.color,
+    cells,
+    targetCell,
+    targetElement,
+    targetFigure,
+  };
+
   switch (startCell.figure.type) {
     case "pawn":
-      return pawnMove(
-        start,
-        target,
-        startCell.figure.color,
-        cells,
-        targetCell,
-        targetElement,
-        targetFigure
-      );
+      return pawnMove(moveParams);
     case "rook":
-      return rookMove(start, target, cells);
+      return rookMove(moveParams);
     case "knight":
-      return knightMove(start, target);
+      return knightMove(moveParams);
     case "bishop":
-      return bishopMove(start, target, cells);
+      return bishopMove(moveParams);
     case "king":
-      return kingMove(start, target);
+      return kingMove(moveParams);
     case "queen":
-      return queenMove(start, target, cells);
+      return queenMove(moveParams);
     default:
       return false;
   }
 };
 
-const queenMove = (
-  start: Coordinate,
-  target: Coordinate,
-  cells: Map<string, Cell>
-) => {
-  return bishopMove(start, target, cells) || rookMove(start, target, cells);
+const queenMove = (move: MoveParams) => {
+  return bishopMove(move) || rookMove(move);
 };
-const kingMove = (start: Coordinate, target: Coordinate) => {
+
+const kingMove = (move: MoveParams) => {
+  const { start, target, color, targetCell, targetElement, targetFigure } =
+    move;
   //todo:  под боем или король рядом?
-  return (
+  if (
     Math.abs(start.row - target.row) <= 1 &&
     Math.abs(start.column - target.column) <= 1
-  );
+  ) {
+    if (!targetFigure) return true;
+    return eatFigure(targetCell, color, targetElement, targetFigure);
+  }
 };
-const knightMove = (start: Coordinate, target: Coordinate) => {
-  return (
+
+const knightMove = (move: MoveParams) => {
+  const { start, target, color, targetCell, targetElement, targetFigure } =
+    move;
+  if (
     (Math.abs(start.column - target.column) === 2 &&
       Math.abs(start.row - target.row) === 1) ||
     (Math.abs(start.column - target.column) === 1 &&
       Math.abs(start.row - target.row) === 2)
-  );
-};
-const bishopMove = (
-  start: Coordinate,
-  target: Coordinate,
-  cells: Map<string, Cell>
-) => {
-  return (
-    Math.abs(start.column - target.column) ===
-      Math.abs(start.row - target.row) &&
-    noFiguresDiagonally(start, target, cells)
-  );
+  ) {
+    if (!targetFigure) return true;
+    return eatFigure(targetCell, color, targetElement, targetFigure);
+  }
 };
 
-const rookMove = (
-  start: Coordinate,
-  target: Coordinate,
-  cells: Map<string, Cell>
-) => {
-  return (
-    (start.row === target.row || start.column === target.column) &&
-    noFiguresStraight(start, target, cells)
-  );
+const bishopMove = (move: MoveParams) => {
+  const {
+    start,
+    target,
+    color,
+    cells,
+    targetCell,
+    targetElement,
+    targetFigure,
+  } = move;
+  const diagonally =
+    Math.abs(start.column - target.column) === Math.abs(start.row - target.row);
+
+  if (!diagonally) return false;
+
+  if (noFiguresDiagonally(start, target, cells) === 0) return true;
+
+  if (
+    noFiguresDiagonally(start, target, cells) === 1 &&
+    eatFigure(targetCell, color, targetElement, targetFigure)
+  )
+    return true;
+
+  return false;
 };
 
-const pawnMove = (
-  start: Coordinate,
-  target: Coordinate,
-  color: string,
-  cells: Map<string, Cell>,
-  enemy: Cell,
-  targetElement: HTMLElement,
-  targetFigure: HTMLElement | null
-) => {
+const rookMove = (move: MoveParams) => {
+  const {
+    start,
+    target,
+    color,
+    cells,
+    targetCell,
+    targetElement,
+    targetFigure,
+  } = move;
+  const straightLine =
+    start.row === target.row || start.column === target.column;
+
+  if (figuresStraight(start, target, cells) === 0) {
+    return straightLine;
+  } else if (
+    figuresStraight(start, target, cells) === 1 &&
+    straightLine &&
+    eatFigure(targetCell, color, targetElement, targetFigure)
+  ) {
+    return start.row === target.row || start.column === target.column;
+  } else return false;
+};
+
+const pawnMove = (move: MoveParams) => {
+  const {
+    start,
+    target,
+    color,
+    cells,
+    targetCell,
+    targetElement,
+    targetFigure,
+  } = move;
+
   const pathRow = target.row - start.row;
   const pathColumn = target.column - start.column;
   const forward = color === "black" ? -1 : 1;
 
   if (pathColumn === 0) {
-    if (!noFiguresStraight(start, target, cells)) return false;
+    if (figuresStraight(start, target, cells) !== 0) return false;
     if (
       (start.row === 6 && color === "black") ||
       (start.row === 1 && color === "white")
@@ -112,16 +162,25 @@ const pawnMove = (
       return pathRow * forward === 1;
     }
   } else if (
-    enemy.figure &&
-    enemy.figure.color !== color &&
     Math.abs(pathColumn) === 1 &&
     pathRow * forward === 1 &&
-    targetFigure
+    eatFigure(targetCell, color, targetElement, targetFigure)
   ) {
-    targetElement.removeChild(targetFigure);
-
     return true;
   } else return false;
+};
+
+const eatFigure = (
+  targetCell: Cell,
+  color: string,
+  targetElement: HTMLElement,
+  targetFigure: HTMLElement | null
+) => {
+  if (targetCell.figure && targetCell.figure.color !== color && targetFigure) {
+    targetElement.removeChild(targetFigure);
+    return true;
+  }
+  return false;
 };
 
 const noFiguresDiagonally = (
@@ -131,17 +190,21 @@ const noFiguresDiagonally = (
 ) => {
   const downDirection = sCoord.row < tCoord.row ? 1 : -1;
   const rightDirection = sCoord.column < tCoord.column ? 1 : -1;
-  let column = sCoord.column;
+  let column = sCoord.column + rightDirection;
   let figureCount = 0;
-  for (let i = sCoord.row; i !== tCoord.row; i = i + downDirection) {
+  for (
+    let i = sCoord.row + downDirection;
+    i !== tCoord.row + downDirection;
+    i = i + downDirection
+  ) {
     const key = `cell-${i}-${column}`;
     column = column + rightDirection;
     if (cells.get(key)?.figure) figureCount++;
   }
-  return figureCount <= 1;
+  return figureCount;
 };
 
-const noFiguresStraight = (
+const figuresStraight = (
   sCoord: Coordinate,
   tCoord: Coordinate,
   cells: Map<string, Cell>
@@ -153,12 +216,12 @@ const noFiguresStraight = (
   const direction = start < target ? 1 : -1;
   let figureCount = 0;
 
-  for (let i = start; i !== target + direction; i = i + direction) {
+  for (let i = start + direction; i !== target + direction; i = i + direction) {
     const key = oneColumn
       ? `cell-${i}-${constCoord}`
       : `cell-${constCoord}-${i}`;
     if (cells.get(key)?.figure) figureCount++;
   }
 
-  return figureCount <= 1;
+  return figureCount;
 };
